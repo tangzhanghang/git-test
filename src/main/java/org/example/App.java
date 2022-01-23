857d4546c6c3f14a17e8843b8b433fb6ff2edc00
hang <1097712132@qq.com>
2022-01-23 15:34:15 +0800

    master1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,10 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World2!" );
-        System.out.println( "Hello World3!" );
-        System.out.println( "Hello Worldhos!" );
-        System.out.println( "Hello Worldhos2!" );
         System.out.println( "master test!" );
     }
 }
