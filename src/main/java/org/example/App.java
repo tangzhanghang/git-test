a8bfc1d1c224db84fe7e981308a76a3c09ecd630
tangzhanghang <1097712132@qq.com>
2022-01-23 12:08:03 +0000

    update src/main/java/org/example/App.java.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,5 +17,6 @@ public class App
         System.out.println( "push test0" );
         System.out.println( "github" );
         System.out.println( "gitee test" );
+        System.out.println( "gitee test2" );
     }
 }
