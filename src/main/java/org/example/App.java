55b813271c33bad486c361f8bd829e7dfb96c8c9
hang <1097712132@qq.com>
2022-01-23 17:15:36 +0800

    push1.1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println( "hot" );
         System.out.println( "master22222222" );
         System.out.println( "hot222222222222" );
-        System.out.println( "push test" );
+        System.out.println( "push test0" );
+        System.out.println( "push test0" );
     }
 }
